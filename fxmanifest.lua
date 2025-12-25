92c61484c42bd48e0c18ed1a99745809554d80b7
wrxew <israel.camposc12@gmail.com>
2025-12-25 11:02:13 -0600

    Create fxmanifest.lua

---
+++ fxmanifest.lua
@@ -0,0 +1,31 @@
+fx_version 'adamant'
+game 'gta5'
+
+author 'RX DEV'
+description 'Avionics'
+version '1.0.0'
+
+lua54 'yes'
+
+client_scripts {
+   'client/**.lua'
+}
+
+server_scripts {
+    'server/**.lua'
+}
+
+shared_scripts {
+   'config.lua',
+   'shared/**.lua'
+}
+
+ui_page 'html/index.html'
+
+files {
+    'html/index.html',
+    'html/**.css',
+    'html/**.js',
+    --'html/img/**.png',
+    'html/sounds/**.mp3'
+}
